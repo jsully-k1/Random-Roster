886dedba096403e979208da202cf97cb5da4fe6a
Jeremiah Sullenger <jeremiah@acroname.com>
2020-07-02 08:59:56 -0600

    Updated to handle MS Teams and Rocket Chat.

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import sys
 import random
 import urllib3
-from os import environ
+from os import getenv
+
 
 def generate_roster(member_list):
     dt = datetime.now()
@@ -19,20 +20,35 @@ def generate_roster(member_list):
         roster.append(name)
 
     scrum_master = member_list[random.randint(0, name_count - 1)]
-    
+
     return scrum_master, roster
 
 
 def render_roster(master, roster):
-
     roster = [f'\t{name}\n' for name in roster]
 
     output = f'Today\'s Scrum Master: {master}\n\n' \
              f'Roster:\n' \
              f'{"".join(roster)}'
     return output
-    
-def slack_post(webhook, body_text):
+
+
+def get_slack_body(body_text):
+    return {'text': body_text}
+
+
+def get_rocket_body(body_text):
+    return {"username": "Random-Roster",
+            "icon_emoji": ":robot:",
+            "text": body_text,
+            }
+
+
+def get_ms_teams_body(body_text):
+    return {'text': body_text}
+
+
+def post(webhook, body):
     http = urllib3.PoolManager()
     exit_code = {
         'statusCode': 200,
@@ -40,10 +56,9 @@ def slack_post(webhook, body_text):
     }
 
     try:
-        slack_data = {'text': body_text}
         response = http.request('POST',
                                 webhook,
-                                body=json.dumps(slack_data),
+                                body=json.dumps(body),
                                 headers={'Content-Type': 'application/json'})
         if response.status != 200:
             raise ValueError(
@@ -54,17 +69,41 @@ def slack_post(webhook, body_text):
     except Exception as e:
         exit_code['statusCode'] = 500
         exit_code['body'] = f'Other Exception: {str(e)}' \
-                            f' (input arguement of --slack_hook : {webhook})'
-                            
+                            f' (input arguement of --url_hook : {webhook})'
+
     return exit_code
-    
+
+
 def lambda_handler(event, context):
-    
-    members = environ["TEAM_ROSTER"]
-    slack_url = environ['SLACK_WEBHOOK_URL']
-    members = members.split(",")
-    
+    exit_code = {
+        'statusCode': 200,
+        'body': 'Success'
+    }
+
+    members = getenv("TEAM_ROSTER", default=None)
+    renders = [{'url': getenv('SLACK_WEBHOOK_URL', default=None), 'render': get_slack_body},
+               {'url': getenv('ROCKET_WEBHOOK_URL', default=None), 'render': get_rocket_body},
+               {'url': getenv('MS_TEAMS_WEBHOOK_URL', default=None), 'render': get_ms_teams_body}]
+
+    if members:
+        members = members.split(",")
+    else:
+        members = []
+
     if len(members):
         master, roster = generate_roster(members)
         roster_output = render_roster(master, roster)
-        return slack_post(slack_url, roster_output)
+
+        for render in renders:
+            if render['url']:
+                body = render['render'](roster_output)
+                ec = post(render['url'], body)
+                if ec['statusCode'] != 200:
+                    exit_code['statusCode'] = 500
+                    exit_code['body'] += ec['body']
+
+    return exit_code
+
+
+if __name__ == '__main__':
+    lambda_handler('a', 'b')
